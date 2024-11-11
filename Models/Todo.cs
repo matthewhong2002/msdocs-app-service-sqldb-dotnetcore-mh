0c2cfa9a18e5488c48203ab614620fca7bb7fe0d
matthewhong2002 <73616818+matthewhong2002@users.noreply.github.com>
2024-11-11 12:45:44 +0000

    changed

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -8,7 +8,7 @@ namespace DotNetCoreSqlDb.Models
         public int ID { get; set; }
         public string? Description { get; set; }
 
-        [DisplayName("Created Date")]
+        [DisplayName("Created Date: ")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
